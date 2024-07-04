80484e1e4ae69867a868b21b7a53e9354a12cf10
sudhanv09 <mystogun125@gmail.com>
2024-07-04 17:49:36 +0800

    coin page and types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,10 +1,43 @@
 export type geckoList = {
-    id: string;
-    symbol: string;
-    name: string;
-    image: string;
-    current_price: number;
-    market_cap: number;
-    market_cap_rank: number;
-    price_change_percentage_24h: number;
-  };
+  id: string;
+  symbol: string;
+  name: string;
+  image: string;
+  current_price: number;
+  market_cap: number;
+  market_cap_rank: number;
+  price_change_percentage_24h: number;
+};
+
+export type CoinData = {
+  id: string;
+  symbol: string;
+  name: string;
+  genesis_date: string;
+  image: string;
+  block_time_in_minutes: number;
+  current_price: number;
+  market_data: MarketCap;
+  market_cap_rank: number;
+};
+
+type MarketCap = {
+  current_price: { [key: string]: number };
+  ath: { [key: string]: number };
+  ath_change_percentage: { [key: string]: number };
+  ath_date: { [key: string]: number };
+  atl: { [key: string]: number };
+  atl_change_percentage: { [key: string]: number };
+  atl_date: { [key: string]: number };
+  market_cap: { [key: string]: number };
+  market_cap_rank: number;
+  high_24h: { [key: string]: number };
+  low_24h: { [key: string]: number };
+
+  price_change_24h: number;
+  price_change_percentage_24h: number;
+  price_change_percentage_7d: number;
+  price_change_percentage_14d: number;
+  price_change_percentage_30d: number;
+  price_change_percentage_1y: number;
+};
--- src/routes/coin/[id].tsx
+++ src/routes/coin/[id].tsx
@@ -1,10 +1,36 @@
-import { useParams } from "@solidjs/router"
+import { cache, createAsync, useParams } from "@solidjs/router";
+import { CoinData } from "~/lib/types";
+
+const routeData = cache(async () => {
+  // "use server";
+  const params = useParams();
+  const response = await fetch(
+    `https://api.coingecko.com/api/v3/coins/${params.id}?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false`
+  );
+
+  if (!response.ok) {
+    if (response.status === 429) {
+      console.log("Rate limit hit.");
+    } else {
+      console.log(`Error: ${response.status} ${response.statusText}`);
+    }
+    return null;
+  }
+
+  const data = await response.json();
+  return data as CoinData;
+}, "coin");
+
+export const route = {
+  load: () => routeData(),
+};
 
 export default function CoinId() {
-    const params = useParams()
-    return (
-        <div>
-            Hello
-        </div>
-    )
+  const coin = createAsync(() => routeData());
+  return (
+    <div class="flex justify-center items-center h-svh">
+      <h1>Hello</h1>
+      <h1>{coin()?.name}</h1>
+    </div>
+  );
 }
